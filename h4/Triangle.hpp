5be3ceb63d4c59f9bc3b6c8873ed871c491274d0
Rangel Plachkov <46283510+Rangel-Plachkov@users.noreply.github.com>
2025-06-25 22:33:36 +0300

    Update Triangle.hpp

--- h4/Triangle.hpp
+++ h4/Triangle.hpp
@@ -6,11 +6,17 @@ class Triangle3D {
 private:
     Vector3D _v0, _v1, _v2;
 
-public:
+public: 
     Triangle3D(const Vector3D& v0, const Vector3D& v1, const Vector3D& v2): _v0(v0), _v1(v1), _v2(v2) {}
 
     Vector3D v0() const { return _v0; }
     Vector3D v1() const { return _v1; }
     Vector3D v2() const { return _v2; }
+
+    Vector3D normal() const {
+        Vector3D edge1 = _v1 - _v0;
+        Vector3D edge2 = _v2 - _v0;
+        return edge1.crossProduct(edge2);
+    }
 
 };
